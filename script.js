08b9a7f675a08a3acc486c4ea3d7836c9be440d0
Kaveri <kaverisg.tce@gmail.com>
2022-04-21 18:12:22 +0530

    Added media property to Lets connect page

--- script.js
+++ script.js
@@ -9,6 +9,19 @@ document.body.classList.toggle("dark-theme");
    icon.src = "moon.png";
 }
 }
+function sendEmail(){
+    Email.send({
+        Host : "smtp.yourisp.com",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
 // function message(){
 // document.getElementById("wrapper").innerHTML = 'Your message has been sent successfully';
 // }
